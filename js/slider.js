f6b18f30ac6128c199dbb8f15c0485ad84f70a5d
Xander Taylor <xanderoid@gmail.com>
2014-04-04 11:12:33 -0700

    made static files, added slider

---
+++ js/slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.bxslider').bxSlider({
+  	auto: true,
+  	autoControls: true,
+  	autoHover: true,
+  	pause: 6000,
+  	controls: false,
+  	randomStart: true,
+  });
+});
